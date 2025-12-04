116e6f45481136388ac1ca0007bca1a567ec7014
RiccardoMiolato <riccardomiolato@gmail.com>
2025-12-04 23:20:30 +0100

    Init solitaire logic

---
+++ solitaire/deck_adapter/main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI, HTTPException
+import requests
+
+app = FastAPI()
+api_url = "https://deckofcardsapi.com/api/deck"
+
+DEBUG_DECK = "h6vgbxtu7jhc"
+
+decks = []
+
+@app.get("/new_deck")
+def get_new_deck():
+    """
+    Retrive a new shuffled deck of cards from the Deck of Cards API
+    """
+    response = requests.get(f"{api_url}/new/shuffle/?deck_count=1")
+
+    deck = response.json()
+
+    decks.append(deck['deck_id'])
+    return deck
+
+@app.get("/draw_cards/{deck_id}/{count}")
+def draw_cards(deck_id: str, count: int):
+    """
+    Draw a specified number of cards from a given deck
+    """
+    print(decks)
+    if deck_id not in decks and deck_id != DEBUG_DECK:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+    response = requests.get(f"{api_url}/{deck_id}/draw/?count={count}")
+
+    return response.json()
+
+@app.post("/shuffle/{deck_id}")
+def shuffle_deck(deck_id: str):
+    """
+    Shuffle the specified deck
+    """
+    if deck_id not in decks and deck_id != DEBUG_DECK:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+    response = requests.get(f"{api_url}/{deck_id}/shuffle/")
+
+    return response.json()
