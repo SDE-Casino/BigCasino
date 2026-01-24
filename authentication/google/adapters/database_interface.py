47f200f3f4bf03687044063ae9e0fe232d207465
Mattia Ferretti <mattia.ferretti@studenti.unitn.it>
2026-01-24 12:04:22 +0100

    Fixed google login after logout

--- authentication/google/adapters/database_interface.py
+++ authentication/google/adapters/database_interface.py
@@ -1,9 +1,21 @@
 from data.database_service import database_service
+from data.models.users import User
 from typing import Optional
 
 class DatabaseInterface:
     def __init__(self):
         self.db_service = database_service
+
+    def _user_to_dict(self, user: User) -> dict:
+        """
+        Convert a SQLAlchemy User object to a dictionary
+        """
+        return {
+            '_id': user.id,
+            'email': user.google_email,
+            'name': user.username,
+            'google_id': user.google_id,
+        }
 
     def find_or_create_user_from_google(self, google_profile: dict) -> dict:
         """
@@ -23,7 +35,9 @@ class DatabaseInterface:
                     'email': email,
                     'name': google_profile.get('name'),
                 })
+                return user  # create_user already returns a dict
 
-        return user
+        # Convert SQLAlchemy User object to dict
+        return self._user_to_dict(user)
 
 database_interface = DatabaseInterface()
